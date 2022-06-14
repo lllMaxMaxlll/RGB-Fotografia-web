fb4d39da8f78895fd49f6fcf07c4d0dca385f4aa
lllmaxmaxlll <max.herr.88@gmail.com>
2022-06-14 17:56:59 -0300

    agregado SoftScroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // Cambio de header
+    // Cambio de header y color de burger
     $(window).on('scroll', function() {
         let posicion = $('#pr').offset();
         if ($(window).scrollTop() >= posicion.top) {
@@ -8,8 +8,18 @@ $(document).ready(function() {
             $('header,#burger').addClass('transparente');
         }
     })
+    // Accionamiento de menu lateral
     $('#burger').click(function(){
         $('#burger').toggleClass('abierto');
         $('.menu').toggleClass('abierto');
+    })
+    // Softscroll
+    $('a').on('click', function() {
+        if(this.hash !== ''){
+            let hash = this.hash
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800)
+        }
     })
 })
